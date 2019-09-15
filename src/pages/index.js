cc47d8dd592882132b64ba4334996daf0d779c30
shadowsyntax <tibetianwolf@gmail.com>
2019-09-15 22:49:30 +0100

    Step 4, MDX blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello London town</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello London town</p>
+      <Link to="/about/">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+
+        // To test the data being pulled from filesystem or api
+        /* <pre>{JSON.stringify(post, null, 2)}</pre> */
+      ))}
+    </Layout>
+  );
+};
